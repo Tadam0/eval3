f77a9f8ca5f095c52c750987742cc3a763d309f3
Tadam0 <triki.adam.lgf@gmail.com>
2026-02-20 10:20:05 +0000

    Ajout dune fonction pour afficher la borne la plus proche

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,32 @@
 using namespace std;
 
 void afficherBornes(const float puiss[], const float dist[], int taille);
+int indice(const float dist[], int taille);
+
+int main() {
+  float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4}; // en kW
+  float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3};    // en km
+  afficherBornes(puissances, distances, 5);
+  indice(distances, 5);
+  return 0;
+}
 
 void afficherBornes(const float puiss[], const float dist[], int taille) {
   for (int i = 0; i < taille; i++) {
-    cout<<"Borne "<<i+1<<" : "<<puiss[i]<<"kW,  à "<<dist[i]<<"km"<<endl;
+    cout << "Borne " << i + 1 << " : " << puiss[i] << "kW,  à " << dist[i]
+         << "km" << endl;
   }
 }
 
-int main() {
-  float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4}; //en kW
-  float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3}; //en km
-  afficherBornes(puissances, distances, 5);
-  return 0;
-} 
-
+int indice(const float dist[], int taille) {
+  int min = 0;
+  int min2 = 0;
+  for (int i = 0; i < taille-1; i++) {
+    min = dist[i];
+    min2 = dist[i+1];
+    if (min<min2)
+      min2=min;
+  }
+   cout<<"La borne la plus proche est à : "<<min2<<"km"<<endl;
+  return min2;
+}
