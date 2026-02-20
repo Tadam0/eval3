1de11053eba372463a0b8f474b3b937446deb8c6
Tadam0 <triki.adam.lgf@gmail.com>
2026-02-20 10:03:37 +0000

    affichage selon une fonction

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
+
+void afficherBornes(const float puiss[], const float dist[], int taille);
+
+void afficherBornes(const float puiss[], const float dist[], int taille) {
+  for (int i = 0; i < taille; i++) {
+    cout<<"Borne "<<i+1<<" : "<<puiss[i]<<"kW,  à "<<dist[i]<<"km"<<endl;
+  }
+}
+
 int main() {
   float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4}; //en kW
   float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3}; //en km
-  for (int i = 0; i < 5; i++) {
-    cout<<"Borne "<<i+1<<" : "<<puissances[i]<<"kW,  à "<<distances[i]<<"km"<<endl;
-  }
+  afficherBornes(puissances, distances, 5);
   return 0;
 } 
+
